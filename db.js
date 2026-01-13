5a312aa5e6bcc36983c95b44b9bb43baf1ca5504
Muskan Kesharwani <muskankesharwani@sigma.in>
2026-01-13 13:02:07 +0530

    second change route

--- db.js
+++ db.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 //Define the mongoose connection URL
-const mongoURL = process.env.MONGODB_URL_LOCAL //Replace 'hotels' with your database name
-// const mongoURL = process.env.MONGODB_URL;
+//const mongoURL = process.env.MONGODB_URL_LOCAL //Replace 'hotels' with your database name
+ const mongoURL = process.env.MONGODB_URL;
 
 //Set up  mongodb connections
 mongoose.connect(mongoURL)
